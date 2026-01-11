fe9c08b1e1e4e471b4413b1b02860fbb2711c187
DJ <dj.leamen@ontariotechu.net>
2026-01-11 14:49:53 -0500

    scan parentheses

--- app/main.py
+++ app/main.py
@@ -24,10 +24,14 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    if file_contents:
-        raise NotImplementedError("Scanner not implemented")
-    else:
-        print("EOF  null") # Placeholder, replace this line when implementing the scanner
+    # Tokenize the file contents
+    for char in file_contents:
+        if char == '(':
+            print("LEFT_PAREN ( null")
+        elif char == ')':
+            print("RIGHT_PAREN ) null")
+
+    print("EOF  null")
 
 if __name__ == "__main__":
     main()
